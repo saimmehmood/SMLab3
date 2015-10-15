1f44e9accd56e44a8ac62a77fa5424d0a18f42f5
saimmehmood <bsef12m557@pucit.edu.pk>
2015-10-15 10:19:48 +0200

    removing unnecessary indentation

--- tweets.py
+++ tweets.py
@@ -18,21 +18,21 @@ def count():
 	han = hon = den = det = denna = denne = hen = 0;
 	for i in range(0, 20):
 		fileinput = urllib.urlopen("http://smog.uppmax.uu.se:8080/swift/v1/tweets/tweets_"+str(i)+".txt")
-		    for line in fileinput:
-				if "han" in line:
-		    			han += 1
-		       		if "hon" in line:
-		    			hon += 1
-				if "den" in line:
-		    			den += 1
-				if "det" in line:
-		    			det += 1
-				if "denna" in line:
-		    			denna += 1
-				if "denne" in line:
-		    			denne += 1
-				if "hen" in line:
-		   			hen += 1
+		for line in fileinput:
+			if "han" in line:
+		    		han += 1
+		       	if "hon" in line:
+		    		hon += 1
+			if "den" in line:
+		    		den += 1
+			if "det" in line:
+		    		det += 1
+			if "denna" in line:
+		    		denna += 1
+			if "denne" in line:
+		    		denne += 1
+			if "hen" in line:
+		   		hen += 1
 		   			
 	print ("han -",han)
 	print ("hon -",hon)
