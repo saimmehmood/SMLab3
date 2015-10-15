cf9355cffbeba70c75da843acd5f5f3717e4b83c
saimmehmood <bsef12m557@pucit.edu.pk>
2015-10-15 08:59:32 +0000

    code change to previous one

--- tweets.py
+++ tweets.py
@@ -1,14 +1,6 @@
-#!flask/bin/python
-from flask import Flask, jsonify
 from celery import Celery
 import urllib
-import subprocess
-import sys
-
-app = Flask(__name__)
 app = Celery('tasks', backend='amqp', broker='amqp://')
-
-@app.route('/tweets/api/v1.0/count', methods=['GET'])
 
 @app.task
 def print_hello():
@@ -16,24 +8,22 @@ def print_hello():
 @app.task
 def count():
 	han = hon = den = det = denna = denne = hen = 0;
-	for i in range(0, 20):
-		fileinput = urllib.urlopen("http://smog.uppmax.uu.se:8080/swift/v1/tweets/tweets_"+str(i)+".txt")
-		for line in fileinput:
-			if "han" in line:
-		    		han += 1
-		       	if "hon" in line:
-		    		hon += 1
-			if "den" in line:
-		    		den += 1
-			if "det" in line:
-		    		det += 1
-			if "denna" in line:
-		    		denna += 1
-			if "denne" in line:
-		    		denne += 1
-			if "hen" in line:
-		   		hen += 1
-		   			
+	fileinput = urllib.urlopen("http://smog.uppmax.uu.se:8080/swift/v1/tweets/tweets_19.txt")
+    	for line in fileinput:
+        		if "han" in line:
+            			han += 1
+       			if "hon" in line:
+            			hon += 1
+        		if "den" in line:
+            			den += 1
+        		if "det" in line:
+            			det += 1
+        		if "denna" in line:
+            			denna += 1
+        		if "denne" in line:
+            			denne += 1
+        		if "hen" in line:
+           			hen += 1
 	print ("han -",han)
 	print ("hon -",hon)
 	print ("den -",den)
@@ -41,8 +31,3 @@ def count():
 	print ("denna -",denna)
 	print ("denne -",denne)
 	print ("hen -",hen)
-
-if __name__ == '__main__':
-    
-    app.run(host='0.0.0.0',debug=True)
-    
