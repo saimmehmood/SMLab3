890f7fdd11457d408014abc8da374bcfe8fb14bd
saimmehmood <bsef12m557@pucit.edu.pk>
2015-10-15 12:37:58 +0200

    copy cat_01

--- tweets.py
+++ tweets.py
@@ -1,18 +1,15 @@
 from flask import Flask, jsonify
 from celery import Celery
+import subprocess
+import sys
 import urllib
-app = Celery('tasks', backend='amqp', broker='amqp://')
+
 
 app = Flask(__name__)
 
-app.config['CELERY_BROKER_URL'] = 'redis://localhost:6379/0'
-app.config['CELERY_RESULT_BACKEND'] = 'redis://localhost:6379/0'
-
-celery = Celery(app.name, broker=app.config['CELERY_BROKER_URL'])
-celery.conf.update(app.config)
 @app.route('/tweets/api/v1.0/saysomething', methods=['GET'])
-@celery.task
 def count():
+	
 	han = hon = den = det = denna = denne = hen = 0;
 	fileinput = urllib.urlopen("http://smog.uppmax.uu.se:8080/swift/v1/tweets/tweets_19.txt")
     	for line in fileinput:
@@ -30,10 +27,19 @@ def count():
             			denne += 1
         		if "hen" in line:
            			hen += 1
+	data=subprocess.check_output(
+	[
 	print ("han -",han)
 	print ("hon -",hon)
 	print ("den -",den)
 	print ("det -",det)
 	print ("denna -",denna)
 	print ("denne -",denne)
-	print ("hen -",hen)
+	print ("hen -",hen) ]
+	)
+	return data
+	
+if __name__ == '__main__':
+
+    app.run(host='0.0.0.0',debug=True)
+    
