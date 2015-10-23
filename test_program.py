8b1437820ee5d5647f65847560d7b3d8d931bd77
saimmehmood <bsef12m557@pucit.edu.pk>
2015-10-23 15:54:15 +0200

    Update test_program.py

--- test_program.py
+++ test_program.py
@@ -6,15 +6,15 @@ import tweets as twt
 
 
 def test_program():
-    print "Starting Process"
-		new_task = twt.count.delay()
-
-		while(new_task.status != 'SUCCESS'):
-			time.sleep(10)
-			print (new_task.status)
-			print "Waiting for the task to complete"
-
-		os.system('python flask_example.py ' + sys.argv[1])
+        print "Starting Process"
+	new_task = twt.count.delay()
+	
+	while(new_task.status != 'SUCCESS'):
+		time.sleep(10)
+		print (new_task.status)
+		print "Waiting for the task to complete"
+	
+	os.system('python flask_example.py ' + sys.argv[1])
 
 
 if __name__ == '__main__':
