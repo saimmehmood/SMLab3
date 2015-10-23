0407f48697e5082d0305df95ee6ba5cbf79411ea
saimmehmood <bsef12m557@pucit.edu.pk>
2015-10-23 11:04:38 +0200

    same as cancer

--- flask_example.py
+++ flask_example.py
@@ -8,6 +8,14 @@ if (output_folder[-1] == '/'):
 print output_folder
 app = Flask(__name__, static_folder=output_folder)
 
+def find_items(directory):
+	items = listdir(directory)
+	return items
+
+@app.route('/' + output_folder + '/<path:filename>', methods=['GET', 'POST'])
+def send_file(filename):
+	return send_from_directory(app.static_folder, filename, as_attachment=True)
+	
 @app.route('/', methods=['GET', 'POST'])
 def index():
     return render_template('index.html', directory=find_items(output_folder))
